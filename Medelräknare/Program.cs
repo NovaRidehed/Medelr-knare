b36cb05078a9a55967fdc3b858e842af85469a40
Nova Ridehed <nova.ridehed@skola.sala.se>
2024-03-14 14:38:14 +0100

    Add project files.

---
+++ Medelräknare/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Medelräknare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int antaltal = 0;
+            double summa = 0;
+            
+            while (true)
+            {
+                Console.Write("Skriv ett heltal: ");
+                string talstring = Console.ReadLine();
+                int tal = int.Parse(talstring);
+                antaltal++;
+                summa = summa + tal;
+                double medelvärde = summa / antaltal;
+                Console.WriteLine("Medelvärde: " + medelvärde);
+            }
+            
+        }
+    }
+}
